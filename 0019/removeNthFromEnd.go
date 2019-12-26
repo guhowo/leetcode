ba8960d116459ad1f71744737faae5ad512d7bc0
guhao02 <guhao02@qutoutiao.net>
2019-12-26 19:42:08 +0800

    删除链表倒数第N个节点

--- 0019/removeNthFromEnd.go
+++ 0019/removeNthFromEnd.go
@@ -9,5 +9,26 @@ type ListNode struct {
 }
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	if head == nil {
+		return nil
+	}
 
+	dummyHead := &ListNode{}
+	dummyHead.Next = head
+
+	front, rear := dummyHead, head
+	for i := 0; i < n; i++ {
+		rear = rear.Next
+	}
+	for rear != nil {
+		front = front.Next
+		rear = rear.Next
+	}
+	front.Next = front.Next.Next
+
+	if front == dummyHead {
+		head = head.Next
+	}
+
+	return head
 }
