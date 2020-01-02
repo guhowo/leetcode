32c2b535defee346e9da66b1aa349a9f0fb40993
guhao02 <guhao02@qutoutiao.net>
2020-01-02 23:05:03 +0800

    相同的树

--- 0100/isSameTree.go
+++ 0100/isSameTree.go
@@ -7,24 +7,23 @@ type TreeNode struct {
 }
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	if (p == nil && q != nil) || (p != nil && q != nil) {
-		return false
-	}
-
 	if p == nil && q == nil {
 		return true
 	}
 
+	if p == nil || q == nil {
+		return false
+	}
+
 	if p.Val != q.Val {
 		return false
-	} else {
-		a := isSameTree(p.Left, q.Left)
-		b := isSameTree(p.Right, q.Right)
-		if a == false || b == false {
-			return false
-		} else {
-			return true
-		}
+	}
+
+	if !isSameTree(p.Left, q.Left) {
+		return false
+	}
+	if !isSameTree(p.Right, q.Right) {
+		return false
 	}
 
 	return true
